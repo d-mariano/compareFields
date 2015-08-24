4cfc646a54fb5b542360684350f5835cc6d07f25
d-mariano <dave.mariano1991@gmail.com>
2015-08-24 10:16:01 -0400

    Updated documentation

--- compareFields.js
+++ compareFields.js
@@ -2,23 +2,28 @@
  * Author: Dave Mariano
  *
  * Script compares two fields on the event that either one
- * is changing and the either sets or clears a message.
+ * is changing and then either sets or clears a message.
  */
 
 // Specify event for each field being compared
 document.getElementById('confirm-password').oninput =
-  function(){ onPassChange(); };
+  function(){ onFieldChange(); };
 
 document.getElementById('password').oninput =
-  function(){ onPassChange(); };
+  function(){ onFieldChange(); };
 
 /**
  * Function compares the current value of either field, if they do
- * not comapare then a span is placed under one of the fieldsand 
+ * not compare then a span is placed under one of the fields and
  * an error class is added to the parent div.  If they do compare, 
- * the parent class is removedand the add span is detached.
+ * the parent class is removed and the added span is detached.
+ *
+ * For this scenario the div around the confirm label is given a
+ * class, this class is referred to in a stylesheet.  If you are
+ * using this script in your own application, be advised of the
+ * different classes, ids, and elements your may use.
  */
-function onPassChange(){
+function onFieldChange(){
   
   // Specify message for span to be inserted
   $span = '<span class="help-block">Passwords are not equal</span>';
@@ -30,7 +35,7 @@ function onPassChange(){
   $submitButton = $("button:submit");
   
   
-  // If passwords match, remove span and revert styles
+  // If passwords match, remove span and class
   if( $confirmPassword.val() === $password.val() ) {
     // Enable submit
     $submitButton.attr('disabled', false);
@@ -39,7 +44,7 @@ function onPassChange(){
     $confirmDiv.removeClass("has-error");
   }
   
-  // If they do not, create a span and styles,
+  // If they do not, create a span and add class,
   // provided that a span does not already exist
   else if( $existingSpan.length === 0 ) { 
     // Disable submit
